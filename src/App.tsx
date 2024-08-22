2e5253078b5aa7e28ee77e2def911fdf2777e5e8
TranHuy <tran.huy211014@gmail.com>
2024-08-22 23:46:13 +0700

    Update Setup Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
+import Heading from './ui/Heading'
+import Row from './ui/Row'
+
 function App() {
-  return <div>Hello Vite</div>
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Natural Hotel</Heading>
+            <div>
+              <Heading as='h2'>Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button variation='secondary' size='small' onClick={() => alert('Check out')}>
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form>
+              <Input type='text' placeholder='Number of guests' />
+              <Input type='text' placeholder='Number of guests' />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  )
 }
+
+const StyledApp = styled.div`
+  padding: 20px;
+`
 
 export default App
